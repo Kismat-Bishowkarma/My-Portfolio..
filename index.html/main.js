c6e0c8c50a4bf879cc496ec859ebe748387bdd90
Kismat-Bishowkarma <xunarkismat@gmail.com>
2025-07-23 13:06:04 +0545

    main.js

--- index.html/main.js
+++ index.html/main.js
@@ -1,19 +1,1 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header navbar a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offssetTop;
-        let height = sec.offsetHeight;
-        let class = sec.getAttribute('class');
-
-
-        if (top >=offset && top < offset + height) {
-            navLinks.forEach(links =>  {
-                links.classList.remove('active');
-                document.querySelector('header navbar a [href*=' + id + ']').classic.add('active');
-            });
-        };
-    });
-};
